5d6eb045391f578add53195ec2b734eabb0e996f
Nikita <mykyta.klymenko@studenci.collegiumwitelona.pl>
2026-06-17 10:21:00 +0200

    Move student registration route to api.php and register API routing in bootstrap

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Auth\StudentRegistrationController;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post("/register/student", StudentRegistrationController::class)->name("register.student");
 
 Route::middleware("auth:sanctum")->get("/user", fn(Request $request): JsonResponse => new JsonResponse($request->user()));
