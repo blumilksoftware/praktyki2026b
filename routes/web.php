7d8aa96180db183e8599297cb7b0aecb30445db5
krzysztofkozyra021 <cysiuu021@protonmail.com>
2026-06-19 09:12:59 +0200

    Refactor routes to auth.php

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Auth\CompanyRegistrationController;
-use App\Http\Controllers\Auth\EmailVerificationController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\StudentRegistrationController;
-use App\Http\Controllers\Auth\UniversityRegistrationController;
 use App\Http\Controllers\Company\CompanyDashboardController;
 use App\Http\Controllers\University\UniversityDashboardController;
 use App\Http\Middleware\EnsureCompanyIsVerified;
@@ -16,32 +11,16 @@ use Inertia\Response;
 
 Route::get("/", fn(): Response => inertia("Welcome"));
 
-Route::post("/register/company", CompanyRegistrationController::class)
-    ->middleware("throttle:10,15")
-    ->name("register.company");
-
 Route::middleware(["auth", EnsureCompanyIsVerified::class])
     ->prefix("company")
     ->group(function (): void {
         Route::get("/dashboard", CompanyDashboardController::class)->name("company.dashboard");
     });
-Route::get("/login", [LoginController::class, "show"])->name("login");
-Route::post("/login", [LoginController::class, "store"])->name("login.store");
-
-Route::get("/register/student", fn(): Response => inertia("Auth/RegisterStudent"))->name("register.student.show");
-Route::post("/register/student", StudentRegistrationController::class)->name("register.student");
-
-Route::get("/email/verify/{id}/{token}", [EmailVerificationController::class, "verify"])
-    ->name("verification.verify");
-Route::post("/email/resend", [EmailVerificationController::class, "resend"])
-    ->name("verification.resend");
-
-Route::post("/register/university", UniversityRegistrationController::class)
-    ->middleware("throttle:10,15")
-    ->name("register.university");
 
 Route::middleware(["auth", EnsureUniversityIsVerified::class])
     ->prefix("university")
     ->group(function (): void {
         Route::get("/dashboard", UniversityDashboardController::class)->name("university.dashboard");
     });
+
+require __DIR__ . "/auth.php";
