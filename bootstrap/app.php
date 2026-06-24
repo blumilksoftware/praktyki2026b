ef6b14b262bfe037520fcdc9c282620bcd49b191
krzysztofkozyra021 <cysiuu021@protonmail.com>
2026-06-24 09:15:22 +0200

    Add missing import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
