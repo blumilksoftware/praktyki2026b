8337b69f4432f23a766db802284069b6e1dd110b
SlomkaGrzegorz <grzegorzslomkaprivate@gmail.com>
2026-07-29 22:08:05 +0200

    share user favorite offer IDs globally

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -7,6 +7,7 @@ namespace App\Http\Middleware;
 use App\Actions\Onboarding\GetProfileStepsAction;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use App\Enums\UserRole;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -29,6 +30,7 @@ class HandleInertiaRequests extends Middleware
             "auth" => fn() => $request->user() ? [
                 "user" => $request->user(),
             ] : null,
+            "favouriteOffersID" => fn() => $request->user()?->role === UserRole::Student ? $request->user()->favourites()->pluck("offers.id")->all() : [],
             "onboarding" => fn() => $request->user() ? $this->onboardingData($request) : null,
             "flash" => [
                 "requires_verification" => $request->session()->get("requires_verification"),
