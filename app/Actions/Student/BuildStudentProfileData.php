7a3b98aa5b82f923c0d357a349ea9854e43ab403
SlomkaGrzegorz <grzegorzslomkaprivate@gmail.com>
2026-08-11 13:46:34 +0200

    Refactor props to camelCase

--- app/Actions/Student/BuildStudentProfileData.php
+++ app/Actions/Student/BuildStudentProfileData.php
@@ -71,8 +71,8 @@ class BuildStudentProfileData
                 "work_modes" => $student->work_modes ?? [],
             ],
             "study_fields" => $studyFields,
-            "university_organization" => $student->universityOrganization ? $mapUniversity($student->universityOrganization) : null,
-            "suggested_university" => $suggestedUniversity ? $mapUniversity($suggestedUniversity) : null,
+            "universityOrganization" => $student->universityOrganization ? $mapUniversity($student->universityOrganization) : null,
+            "suggestedUniversity" => $suggestedUniversity ? $mapUniversity($suggestedUniversity) : null,
         ];
     }
 }
