d9c24ca48dd76e5fac6520fc6ba254f7cf14db08
Mallinowo <jakub.malinski@studenci.collegiumwitelona.pl>
2026-08-27 09:51:45 +0200

    cities and industry tags schema

---
+++ app/Models/City.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class City extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        "name",
+    ];
+}
--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -9,8 +9,10 @@ use App\Enums\UserRole;
 use App\Enums\UserStatus;
 use App\Enums\WorkMode;
 use App\Models\Application;
+use App\Models\City;
 use App\Models\Company;
 use App\Models\Faculty;
+use App\Models\IndustryTag;
 use App\Models\Offer;
 use App\Models\StudyField;
 use App\Models\University;
@@ -193,6 +195,14 @@ class DemoSeeder extends Seeder
 
         foreach ($testApplications as $testApplication) {
             Notification::send($companyAdmin, new NewApplicationNotification($testApplication));
+        }
+
+        foreach (array_keys($this->citiesWithCoordinates) as $cityName) {
+            City::query()->firstOrCreate(["name" => $cityName]);
+        }
+
+        foreach (["IT", "Software house", "Programowanie", "Praca zdalna", "Vue.js", "Python"] as $tagName) {
+            IndustryTag::query()->firstOrCreate(["name" => $tagName]);
         }
 
         $faculty = Faculty::factory()->for($approvedUniversity)->create([
