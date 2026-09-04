606544d969c692347baf3324d5a7d313cebcb85a
stanislavzhukk <stanislav.zhuk@studenci.collegiumwitelona.pl>
2026-09-04 09:18:42 +0200

    dd.mm.yyyy

--- resources/js/Components/Admin/AdminPreviewUserModal.vue
+++ resources/js/Components/Admin/AdminPreviewUserModal.vue
@@ -28,7 +28,8 @@ function fullName(user) {
 }
 function formattedDate(date) {
   if (!date) return ''
-  return new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(new Date(date))
+  return new Intl.DateTimeFormat('pl-PL', { day: '2-digit', month: '2-digit', year: 'numeric' })
+    .format(new Date(date))
 }
 
 const details = computed(() => {
